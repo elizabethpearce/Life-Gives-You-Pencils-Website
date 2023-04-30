1d7cc32709742242925b399b01de95d89b0765f0
Elizabeth Pearce <eliz.mpearce@gmail.com>
2023-04-30 16:57:21 -0500

    Art Section Home Page Finished

--- src/pages/art-section.js
+++ src/pages/art-section.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
+import imageOne from '../static/assets/images/Artwork_Homepage_Spot_1.jpg'
+import imageTwo from '../static/assets/images/Artwork_Homepage_Spot_2.jpg'
 
 export default class ArtSection extends Component {
   render() {
     return (
       <div className='art-section'>
         <div className='art-spot-one'>
-          Image One
-          <div><button className='btn'>For Sale</button></div>
+          <img class='image-one' src={imageOne} alt='For Sale'></img>
+          <div className='for-sale-btn-wrapper'><button className='btn'>For Sale</button></div>
         </div>
 
         <div className='art-spot-2'>
-          Image Two
-          <div><button className='btn'>All Artwork</button></div>
+          <img class='image-two'src={imageTwo} alt='All Artwork'></img>
+          <div className='all-artwork-button-wrapper'><button className='btn'>All Artwork</button></div>
         </div>
       </div>
     );
