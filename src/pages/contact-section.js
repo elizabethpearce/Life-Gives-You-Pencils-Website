274753a0a52af4b25c809c548a73176fdb5abd84
Elizabeth Pearce <eliz.mpearce@gmail.com>
2023-04-30 17:15:44 -0500

    Home Page Done

--- src/pages/contact-section.js
+++ src/pages/contact-section.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react';
 export default class ContactSection extends Component {
   render() {
     return (
-      <div>
-        <button className='btn-contact-wrapper'>
+      <div className='btn-contact-wrapper'>
+        <button className='btn-contact'>
           <div>Contact</div>
           <div>Email: info@lifegivesyoupencils.com</div>
           <div>Mailing:</div>
           <div>P.O. Box 536</div>
+          <div>Peyton, CO 80831</div>
         </button> 
       </div>
     );
