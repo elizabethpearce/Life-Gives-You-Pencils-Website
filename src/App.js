e7cdd7a121d4e86b408c3f2a9356bc94414bfe0a
Elizabeth Pearce <eliz.mpearce@gmail.com>
2023-05-01 19:42:07 -0500

    push before attempting to add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from "./pages/home.js";
 import './main.scss';
 
 export default class App extends Component {
   render() {
-    return (
-      <div className='app'>
-        <Home/>
-      </div>
+    return (   
+      
+          <div className='app'>
+         <Home/>
+     
+        </div>
+     
+
     );
   }
 }
