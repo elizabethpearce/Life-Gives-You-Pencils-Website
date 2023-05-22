e1aa494931d22d8fe470e8bea4a05effad7d7396
Elizabeth Pearce <eliz.mpearce@gmail.com>
2023-05-21 20:14:33 -0500

    Events and News Page Setup Complete

--- src/pages/events-and-news.js
+++ src/pages/events-and-news.js
@@ -6,7 +6,7 @@ import ContactSection from './components/home/contact-section';
 const EventsAndNews = () => {
  
     return (
-      <div className='about-page-wrapper'>
+      <div className='events-and-news-wrapper'>
       <div className='nav-bar-header-wrapper'>
         <div className='logo-wrapper'>
           <NavBarLogo />
@@ -15,8 +15,34 @@ const EventsAndNews = () => {
           <NavBar />
         </div>
       </div>
-      <div>
-        Events and News!
+        <div className='events-and-news-header-wrapper'>
+          - Events & News -
+        </div>
+        <div className='events-and-news-text-wrapper'>
+        <div className='events-wrapper'>
+          Current Events Listing<br/><br/>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet <br/>
+          facilisis ligula. Sed pellentesque eu tellus sed aliquet. Nullam justo augue, <br/>
+          iaculis at luctus at, efficitur sit amet sapien. Vestibulum diam ante, pretium <br/>
+          sit amet metus sed, vulputate porta turpis. In auctor metus at tellus posuere, <br/>
+          eu consequat mauris auctor. Ut nec libero nisl. Morbi elit nunc, <br/>
+          bibendum ac augue ac, egestas ultrices quam. Suspendisse potenti. <br/>
+          In in felis mi. Curabitur molestie, erat sagittis dapibus dapibus, <br/>
+          mi ante porta ante, eget tempor tortor quam aliquet dolor. <br/>
+          Nam arcu libero, feugiat non ex a, ullamcorper aliquet ligula.
+        </div>
+        <div className='news-wrapper'>
+          News Listings<br/><br/>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet <br/>
+          facilisis ligula. Sed pellentesque eu tellus sed aliquet. Nullam justo augue, <br/>
+          iaculis at luctus at, efficitur sit amet sapien. Vestibulum diam ante, pretium <br/>
+          sit amet metus sed, vulputate porta turpis. In auctor metus at tellus posuere, <br/>
+          eu consequat mauris auctor. Ut nec libero nisl. Morbi elit nunc, <br/>
+          bibendum ac augue ac, egestas ultrices quam. Suspendisse potenti. <br/>
+          In in felis mi. Curabitur molestie, erat sagittis dapibus dapibus, <br/>
+          mi ante porta ante, eget tempor tortor quam aliquet dolor. <br/>
+          Nam arcu libero, feugiat non ex a, ullamcorper aliquet ligula.
+        </div>
       </div>
       <div className='contact-section-wrapper'>
         <ContactSection />
